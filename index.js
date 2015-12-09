952c795e7cd72a4f3ebf94612ddd55f9dd9112f0
David Fekke <david@fekke.com>
2015-12-09 08:04:14 -0500

    Added http request to JaxNode API

--- index.js
+++ index.js
@@ -3,10 +3,24 @@
 var http = require('http');
 
 var options = {
-		
+	hostname: 'www.jaxnode.com',
+	port: 80,
+	path: '/api',
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/x-www-form-urlencoded'
+	}
 };
 
 var req = http.request(options, function (response)  {
-	
+	response.setEncoding('utf8');
+	response.on('data', function (chunk) {
+		console.log(chunk);
+	});
+	response.on('end', function() {
+		
+	});
 });
-+ console.log('Hello, world!');
+
+req.end();
+console.log('Hello, world!');
