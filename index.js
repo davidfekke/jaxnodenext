da0c2be17c8912dff9510c44a6ee39db3dc8a899
David Fekke <david@fekke.com>
2015-12-10 14:15:19 -0500

    Adding commander framework

--- index.js
+++ index.js
@@ -2,12 +2,33 @@
 
 var open = require('open'),
 	JnnData = require('./lib'),
+	program = require('commander'),
 	nextMeeting;
 
+program
+	.option('-d, --details', 'More details about the meetup')
+	.option('-m, --map', 'Google Map to Meetup location')
+	.option('-l, --link', 'Open the home page url')
+	.option('-a, --address', 'Show the venue address')
+	.parse(process.argv);
+
 JnnData.getNextMeeting(function (nextMeeting) {
-	//var mapurl = 'https://www.google.com/maps/place/' + nextMeeting.meeting.venue.name + '/@' + nextMeeting.meeting.venue.lat + ',' + nextMeeting.meeting.venue.lon + 'z18';
+	var mapurl = 'https://www.google.com/maps/place/' + nextMeeting.meeting.venue.name + '/@' + nextMeeting.meeting.venue.lat + ',' + nextMeeting.meeting.venue.lon + 'z18';
 	var meetingText = 'The next JaxNode meeting will be on ' + nextMeeting.meeting.time + ' at ' + nextMeeting.meeting.venue.name;
 	console.log(meetingText);
-	open('http://www.jaxnode.com');	
+	if (program.details) {
+		console.log(nextMeeting.meeting.name + ':' + nextMeeting.meeting.description);
+	}
+	if (program.map) {
+		open(mapurl);
+	}
+	if (program.link) {
+		open('http://www.jaxnode.com');	
+	}
+	if (program.address) {
+		console.log(nextMeeting.meeting.venue.name);
+		console.log(nextMeeting.meeting.venue.address_1);
+		console.log(nextMeeting.meeting.venue.city + ', ' + nextMeeting.meeting.venue.state);
+	}
 });
 
