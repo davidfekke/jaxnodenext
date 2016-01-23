2930e57a036005578d678bf1f4e507ecb45316a5
davidfekke <david@fekke.com>
2016-01-23 17:38:32 -0500

    Adding fix for missing venue

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('nsp', function (cb) {
 });
 
 gulp.task('pre-test', function () {
-    return gulp.src(['index.js', 'lib/**/index.js'])
+    return gulp.src('lib/**/*.js')
     .pipe(istanbul({
         includeUntested: true
     }))
